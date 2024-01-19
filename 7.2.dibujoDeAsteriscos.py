706b1f771a7c246186287f9ebcd88792a62c3111
jorgelpat <jorgelpatino724@gmail.com>
2024-01-19 13:18:05 -0500

    7.2 Dibujo de Triangulo

---
+++ 7.2.dibujoDeAsteriscos.py
@@ -0,0 +1,10 @@
+#Escriba un programa que dibuje el triángulo del tamaño indicado por el usuario de acuerdo al ejemplo
+
+
+
+alt = int(input("Altura: "))
+
+for i in range(alt):
+    output = (i + 1)*"*"
+    print(output)
+
